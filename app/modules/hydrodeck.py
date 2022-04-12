f0a4d2277b261c4a4d6886f2bcce6b40ef8bb4af
Jelle Maas <typiqally@gmail.com>
2022-04-12 14:04:32 +0200

    Add hydrodeck setup function

--- app/modules/hydrodeck.py
+++ app/modules/hydrodeck.py
@@ -6,8 +6,29 @@ try:
     import RPi.GPIO as GPIO
     import app.config as config
 
+    output_pins = [
+        config.OPEN_GPIO_PIN,
+        config.CLOSE_GPIO_PIN,
+        config.STOP_GPIO_PIN,
+    ]
+
     # Update controllable status
     is_controllable = True
+
+
+    def setup():
+        GPIO.setmode(GPIO.BOARD)
+
+        for pin in output_pins:
+            setup_output(pin)
+
+
+    def setup_output(pin):
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+
+
+    setup()
 
 except ImportError:
     logging.warning("Unable to import RPi.GPIO package, "
